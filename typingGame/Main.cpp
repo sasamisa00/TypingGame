9dc4e96aff89b4d43e38a0e2c0014cdc87c6928f
sasamisa00 <mm2362km@gm.tsuda.ac.jp>
2024-09-08 13:20:26 +0900

    make enum of cylliric chara

--- typingGame/Main.cpp
+++ typingGame/Main.cpp
@@ -2,16 +2,132 @@
 #include "wordList.h"
 
 
+Array<String> CyrillicAlphabetToWords(CyrillicAlphabetList cyrillicChar)
+{
+	Array<String> words;
+
+	switch (cyrillicChar)
+	{
+	case CyrillicAlphabetList::а:
+		words = аWords;
+		break;
+	case CyrillicAlphabetList::б:
+		words = бWords;
+		break;
+	case CyrillicAlphabetList::в:
+		words = вWords;
+		break;
+	case CyrillicAlphabetList::г:
+		words = гWords;
+		break;
+	case CyrillicAlphabetList::д:
+		words = дWords;
+		break;
+	case CyrillicAlphabetList::е:
+		words = еWords;
+		break;
+	case CyrillicAlphabetList::ё:
+		words = ёWords;
+		break;
+	case CyrillicAlphabetList::ж:
+		words = жWords;
+		break;
+	case CyrillicAlphabetList::з:
+		words = зWords;
+		break;
+	case CyrillicAlphabetList::и:
+		words = иWords;
+		break;
+	case CyrillicAlphabetList::й:
+		words = йWords;
+		break;
+	case CyrillicAlphabetList::к:
+		words = кWords;
+		break;
+	case CyrillicAlphabetList::л:
+		words = лWords;
+		break;
+	case CyrillicAlphabetList::м:
+		words = мWords;
+		break;
+	case CyrillicAlphabetList::н:
+		words = нWords;
+		break;
+	case CyrillicAlphabetList::о:
+		words = оWords;
+		break;
+	case CyrillicAlphabetList::п:
+		words = пWords;
+		break;
+	case CyrillicAlphabetList::р:
+		words = рWords;
+		break;
+	case CyrillicAlphabetList::с:
+		words = сWords;
+		break;
+	case CyrillicAlphabetList::т:
+		words = тWords;
+		break;
+	case CyrillicAlphabetList::у:
+		words = уWords;
+		break;
+	case CyrillicAlphabetList::ф:
+		words = фWords;
+		break;
+	case CyrillicAlphabetList::х:
+		words = хWords;
+		break;
+	case CyrillicAlphabetList::ц:
+		words = цWords;
+		break;
+	case CyrillicAlphabetList::ч:
+		words = чWords;
+		break;
+	case CyrillicAlphabetList::ш:
+		words = шWords;
+		break;
+	case CyrillicAlphabetList::щ:
+		words = щWords;
+		break;
+	case CyrillicAlphabetList::ъ:
+		// words = ъWords;
+		break;
+	case CyrillicAlphabetList::ы:
+		// words = ыWords;
+		break;
+	case CyrillicAlphabetList::ь:
+		// words = ьWords;
+		break;
+	case CyrillicAlphabetList::э:
+		words = эWords;
+		break;
+	case CyrillicAlphabetList::ю:
+		words = юWords;
+		break;
+	case CyrillicAlphabetList::я:
+		words = яWords;
+		break;
+	default:
+		words = аWords;
+		break;
+	}
+
+	return words;
+}
+
+
 class WordGame
 {
 public:
-	WordGame(const Array<String> words)
-		: words(words)
+
+	WordGame(CyrillicAlphabetList cyrillicChar)
+		: words(CyrillicAlphabetToWords(cyrillicChar))
 	{
 		// 問題文をランダムに選ぶ
 		target = words.choice();
 
 	}
+
 	~WordGame()
 	{
 	}
@@ -81,12 +197,14 @@ void Main()
 {
 	Scene::SetBackground(Palette::Powderblue);
 
+
 	//問題のアルファベットを選ぶ
-	const Array<String> words = вWords;
+	String cyrillicAlphabt;
+	TextInput::UpdateText(cyrillicAlphabt, TextInputMode::DenyControl);
 
 
 
-	WordGame wordGame(words);
+	WordGame wordGame(CyrillicAlphabetList::б);
 
 	while (System::Update())
 	{
