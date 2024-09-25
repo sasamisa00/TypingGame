1aa42bcc51c517edb1b0bddbdc6491f22b45a5ea
sasamisa00 <mm2362km@gm.tsuda.ac.jp>
2024-09-25 14:03:56 +0900

    add sentence file

--- typingGame/FoxBoySentences.h
+++ typingGame/FoxBoySentences.h
@@ -29,11 +29,177 @@ U"油揚げは、昔から、狐の好きな食べ物だと言われています
 U"だから「きつねそば」と言うんです。",
 };
 
+const PageSentence foxBoyPage3 =
+{
+	U"ニノさんは、この町で友だちがほしかったので、マッチングアプリを始めました。",
+
+U"そして、ツネキくんと仲良くなりました。",
+
+};
+
+const PageSentence foxBoyPage4 =
+{
+	U"町の夏祭りで、ニノさんはツネキくんと会いました。",
+
+U"──かっこいい！──",
+
+U"ニノさんは、うれしくなりました。",
+
+
+};
+
+const PageSentence foxBoyPage5 =
+{
+	U"2人で「金魚すくい」をしました。",
+
+};
+
+const PageSentence foxBoyPage6 =
+{
+	U"それから、きつねそばを食べました。",
+
+U"ツネキくんは、油揚げをおいしそうに食べました。",
+
+U"ツネキくんは油揚げが大好きなんだと、ニノさんは思いました。",
+
+
+};
+
+const PageSentence foxBoyPage7 =
+{
+	U"それから「射的」をしました。",
+
+U"ニノさんは狐のおもちゃがほしいと思いました。",
+
+
+
+};
+
+const PageSentence foxBoyPage8 =
+{
+	U"バーン！",
+
+U"ニノさんは鉄砲でおもちゃを撃ちました。",
+
+U"ツネキくんは、びっくりして、狐になってしまいました。",
+
+U"ツネキくんは、本当は狐だったのです。",
+
+
+
+};
+
+
+const PageSentence foxBoyPage9 =
+{
+	U"すぐに、ツネキくんは山の方へ逃げて行きました。",
+
+
+};
+
+const PageSentence foxBoyPage10 =
+{
+	U"ニノさんは電話をしましたが、ツネキくんは電話に出ません。",
+
+U"でも、ニノさんはツネキくんを忘れることができません。",
+
+U"──会いたいな──",
+
+
+
+};
+
+const PageSentence foxBoyPage11 =
+{
+	U"ニノさんは考えました。",
+
+U"──そうだ！ツネキくんは油揚げが好きなんだ──",
+
+U"すぐにニノさんは、おそば屋さんで油揚げをもらいました。",
+
+
+
+};
+
+const PageSentence foxBoyPage12 =
+{
+	U"ニノさんは油揚げを持って、山に行きました。",
+
+
+};
+
+const PageSentence foxBoyPage13 =
+{
+	U"ニノさんは油揚げを置いて、ツネキくんを待ちました。",
+
+U"すると、ツネキくんが来ました。",
+
+
+
+};
+
+const PageSentence foxBoyPage14 =
+{
+	U"「ツネキくん！」",
+
+U"「あ、ニノさん！」",
+
+U"ツネキくんはびっくりして逃げました。",
+
+U"ニノさんはツネキくんの尻尾をつかまえました。",
+
+U"「なんで逃げるの？」",
+
+U"「ぼく、狐だから…。」",
+
+U"「狐の彼氏は嫌でしょう？」",
+
+U"「狐でもいいの。私はツネキくんが好きなの!!」",
+
+
+
+};
+
+const PageSentence foxBoyPage15 =
+{
+	U"「本当？ じゃあ、今度は山でデートしようよ。」",
+
+
+};
+
+const PageSentence foxBoyPage16 =
+{
+	U"2人はもっと仲良くなりました。",
+
+
+
+};
+
+
+
+
 const Array<PageSentence> foxBoySentences =
 {
-	
+	U"",
+	U"",
 	foxBoyPage1,
-	foxBoyPage2
+	foxBoyPage2,
+	foxBoyPage3,
+	foxBoyPage4,
+	foxBoyPage5,
+	foxBoyPage6,
+	foxBoyPage7,
+	foxBoyPage8,
+	foxBoyPage9,
+	foxBoyPage10,
+	foxBoyPage11,
+	foxBoyPage12,
+	foxBoyPage13,
+	foxBoyPage14,
+	foxBoyPage15,
+	foxBoyPage16,
+	U"",
+	U"",
 };
 
 	
