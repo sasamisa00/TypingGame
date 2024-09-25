258679025e5d0edf2f2969f597450fea627ec9f7
sasamisa00 <mm2362km@gm.tsuda.ac.jp>
2024-09-25 12:35:39 +0900

    Revert "Add shorttparis"

--- typingGame/Main.cpp
+++ typingGame/Main.cpp
@@ -4,7 +4,6 @@
 #include "WordGame.h"
 #include "ColorDefinition.h"
 #include "MathHelpers.h"  // 追加
-#include "RedFlagTyping.h"
 
 // このコードは
 // https://siv3d.github.io/ja-jp/samples/games/#5-%E3%82%BF%E3%82%A4%E3%83%94%E3%83%B3%E3%82%B0%E3%82%B2%E3%83%BC%E3%83%A0
@@ -18,8 +17,7 @@ void Main()
 	// ウィンドウサイズを可変に設定
 	Window::SetStyle(WindowStyle::Sizable);
 
-	//WordGame wordGame;
-	RedFlagTyping wordGame;
+	WordGame wordGame;
 	ScreenshotManager screenshotManager(backgroundColor); // スクリーンショットマネージャのインスタンスを作成
 
 	/*const Array<String> words = SelectAdjustedRandomWords(вWords);
